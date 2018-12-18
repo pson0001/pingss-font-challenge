4958b3bffac477b6bfbbb91a6b7f40e239e77d62
pson0001 <pson0001@hotmail.com>
2018-12-19 03:17:19 +1100

    Updates

---
+++ precache-manifest.6505adea1aab966366b344d66c97f58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d89fca40a69931c2e5",
+    "url": "/pingss-font-challenge/static/js/runtime~main.46d89fca.js"
+  },
+  {
+    "revision": "4bd9692ff0470affc05c",
+    "url": "/pingss-font-challenge/static/js/main.4bd9692f.chunk.js"
+  },
+  {
+    "revision": "c84579f7d6fabc5c8256",
+    "url": "/pingss-font-challenge/static/js/1.c84579f7.chunk.js"
+  },
+  {
+    "revision": "4bd9692ff0470affc05c",
+    "url": "/pingss-font-challenge/static/css/main.9c36392a.chunk.css"
+  },
+  {
+    "revision": "c84579f7d6fabc5c8256",
+    "url": "/pingss-font-challenge/static/css/1.95baffc4.chunk.css"
+  },
+  {
+    "revision": "94c3b96dba4ba16ece1394abeba29f5b",
+    "url": "/pingss-font-challenge/index.html"
+  }
+];
